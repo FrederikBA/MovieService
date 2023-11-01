afe546a28d48648b8d894b74be795963c4054500
FrederikBA <frederikbandersen98@gmail.com>
2023-11-01 18:08:57 +0100

    dto json attributes

--- MovieRental.Web/Model/Dto/MovieDto.cs
+++ MovieRental.Web/Model/Dto/MovieDto.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace MovieRental.Web.Model.Dto;
 
 public class MovieDto
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+    [JsonPropertyName("title")]
     public string Title { get; set; } = null!;
 }
